7280bd6aeada6d8d0854d0cbbe3a9d4998435d99
Drew Pham <drewvpham@gmail.com>
2017-05-23 15:19:35 -0500

    added to solution

--- apps/django_books/models.py
+++ apps/django_books/models.py
@@ -2,11 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Book(models.Model):
     title = models.CharField(max_length=38)
-    author = models.CharField(max_length= 200)
     published_date=models.DateTimeField()
-    category = models.CharField(max_length= 100)
+    category = models.CharField(max_length=100)
     in_print = models.BooleanField(default=True)
+    author = models.OneToOneField(Author, related_name='your_book')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
